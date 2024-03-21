8c67d1a692420cc7ddb9f48b0768af3038f62904
Guilherme Araujo <99432207+GuilhermeAraujo062@users.noreply.github.com>
2024-03-21 19:14:58 -0300

    front end finalizado

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Menu burger
+
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {
+    if (itens.style.display == 'none') {
+        intes.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+});
